bbc8829265795b7a4b819a395b3e060c5de6c255
Micol Hebrero <micolhebrero@gmail.com>
2022-05-18 12:52:06 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar} from './componentes/Navbar'
+
 
 function App() {
+
+  const contenido = "Primer contenido de mi App."
+  const estiloContenido = {fontSize: "40x", border: "2px solid #FFF"}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+
+      <p style={estiloContenido}>{contenido}</p>
+
     </div>
   );
 }
---
+++ src/componentes/Navbar.js
@@ -0,0 +1,13 @@
+
+export const Navbar = () => {
+
+    return <header>
+        <h1>MIGAGI TEJIDOS</h1>
+
+        <nav>
+            <p>Accesorios</p>
+            <p>Indumentaria</p>
+            <p>Deco</p>
+        </nav>
+    </header>
+}
