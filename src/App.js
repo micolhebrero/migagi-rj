c3d85978f981377a4760b169df0d2e660badff85
Micol Hebrero <micolhebrero@gmail.com>
2022-05-31 22:12:03 -0300

    contador con botón

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {Navbar} from './componentes/Navbar/Navbar'
 import {ItemListContainer} from './componentes/ItemListContainer/ItemListContainer'
 import {Nosotros} from './componentes/Nosotros/Nosotros'
+import Counter from './componentes/Counter/Counter';
+
 
 function App() {
 
@@ -14,8 +16,11 @@ function App() {
     <div className="App">
       <Navbar/>
 
+     
      <ItemListContainer nombre={integrantes.nombre}/>
      <Nosotros/>
+     <Counter/>
+
 
 
     </div>
---
+++ src/componentes/Counter/Counter.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react" 
+import Button from 'react-bootstrap/Button'
+
+
+const Counter = () => {
+
+    let [contador, setContador] = useState(0)
+
+
+    const incrementar = () => {
+
+        setContador(contador + 1)
+    }
+    const disminuir = () => {
+
+        setContador(contador - 1)
+    }
+
+    return (
+        <div className="container my-5">
+            <h2>Counter</h2>
+            <Button onClick={incrementar}  variant="secondary">+</Button>{' '}
+            <p>{contador}</p>
+            <Button onClick={disminuir}  variant="secondary">-</Button>{' '}
+
+        </div>
+    )
+}
+
+export default Counter
